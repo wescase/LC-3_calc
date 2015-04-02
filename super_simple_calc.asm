f039704bf5765ca5214262c445e45c45d0e4cc8e
Ayne Delgado <aynedelgado@Aynes-MBP.ad.uvu.edu>
2015-04-02 16:39:40 -0600

    added addition and subtraction parts

--- super_simple_calc.asm
+++ super_simple_calc.asm
@@ -1,4 +1,38 @@
 ;By Wes, Morgan, Ayne and Eric
 ; This is the test
 .ORIG x3000
+
+
+
+
+
+
+
+
+
+
+
+;using R2 and R3 as the operands, and putting answer in R2
+;using R4 and R5 as registers where input was stored
+
+ADDITION LDR R2, R4, #0 ;get left hand operand
+	 LDR R3, R5, #0	;get right hand operand
+	 ADD R2, R2, R3	;add R2 + R3 and store in R2
+	;branch back to the output section
+
+
+;using R2 and R3 as the operands, and putting answer in R2
+;using R4 and R5 as registers where input was stored
+
+SUBTRACTION LDR R2, R4, #0 ;load lho
+	    LDR R3, R5, #0 ;load rho
+	    NOT R3, R3     ;make R3 negative
+	    ADD R2, R2, R3 ;add R2 + R3 and store in R2
+	    ;branch back to output the answer
+
 .END
+
+
+
+
+
